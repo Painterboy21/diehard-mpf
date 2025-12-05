08b227160dd979ebb16d4821aae13e143b4d0c83
Matt Carr <mpcarr84@gmail.com>
2025-12-05 20:44:44 +0000

    adds display bullets to lcd

--- slides/bullets.gd
+++ slides/bullets.gd
@@ -2,8 +2,10 @@ extends Control
 
 var bullet_nodes = []
 
-func _ready() -> void:
+@onready var bullets_collected: Label = $BulletsCount
 
+func _ready() -> void:
+	bullets_collected.text = "0 of 15"
 	for i in range(1, 16):
 		var bullet = get_node("%sBullets" % i) as Sprite2D
 		bullet.visible = false
@@ -12,4 +14,11 @@ func _ready() -> void:
 	MPF.game.connect("player_update", self._on_player_update)
 
 func _on_player_update(var_name: String, value: Variant) -> void:
-	print("TODO")
+	if var_name == "bullet_hits":
+		if value > 15:
+			value = 15
+
+		var format_string = "%s of 15"
+		bullets_collected.text = format_string % value
+		for i in range(0,value):
+			bullet_nodes[i].visible = true
