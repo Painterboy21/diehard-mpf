778818c8a52907f5391704bfb1d9e8fb5391fc79
Matt Carr <mpcarr84@gmail.com>
2025-12-06 14:14:08 +0000

    fix bullets on lcd

--- slides/bullets.gd
+++ slides/bullets.gd
@@ -1,15 +1,19 @@
 extends Control
 
 var bullet_nodes = []
-
+var format_string = "%s of 15"
 @onready var bullets_collected: Label = $BulletsCount
 
 func _ready() -> void:
-	bullets_collected.text = "0 of 15"
+	bullets_collected.text = format_string % MPF.game.player.bullet_hits
+
 	for i in range(1, 16):
 		var bullet = get_node("%sBullets" % i) as Sprite2D
 		bullet.visible = false
 		bullet_nodes.append(bullet)
+
+	for i in range(0,MPF.game.player.bullet_hits):
+			bullet_nodes[i].visible = true
 
 	MPF.game.connect("player_update", self._on_player_update)
 
@@ -18,7 +22,6 @@ func _on_player_update(var_name: String, value: Variant) -> void:
 		if value > 15:
 			value = 15
 
-		var format_string = "%s of 15"
 		bullets_collected.text = format_string % value
 		for i in range(0,value):
 			bullet_nodes[i].visible = true
