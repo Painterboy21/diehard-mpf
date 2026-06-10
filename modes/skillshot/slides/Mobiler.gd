bce40f0216a8bbb1229b926eb3add14032dc9673
BOSGAME-1NWVA\finis <finishingtouches.co.uk@googlemail.com>
2026-06-10 23:43:30 +0100

    a lot of fixes

--- modes/skillshot/slides/Mobiler.gd
+++ modes/skillshot/slides/Mobiler.gd
@@ -7,7 +7,22 @@ var hitSuccess = false
 var playingIntro = true
 var skillshotHitSent = false
 
+var playerBallFlashTime = 0.0
+var playerBallFlashSpeed = 0.25
+var playerBallFlashCount = 0
+var playerBallMaxFlashes = 6
+var playerBallFlashFinished = false
+var playerBallLabelActive = false
+
+var playerBallHoldTime = 0.0
+var playerBallHoldDuration = 4.0
+var playerBallHolding = false
+
+var clearSkillshotHandler := Callable()
+
 @onready var videoPlayer: VideoStreamPlayer = $"../VideoStreamPlayer"
+@onready var playerBallLabel: Label = $"../PlayerBallLabel"
+
 
 func _ready():
 	position.x = -312
@@ -16,6 +31,18 @@ func _ready():
 	hitSuccess = false
 	playingIntro = true
 	skillshotHitSent = false
+
+	playerBallFlashTime = 0.0
+	playerBallFlashCount = 0
+	playerBallFlashFinished = false
+	playerBallLabelActive = false
+	playerBallHoldTime = 0.0
+	playerBallHolding = false
+
+	clearSkillshotHandler = Callable(self, "_on_clear_skillshot_player_ball")
+	MPF.server.add_event_handler("clear_skillshot_player_ball", clearSkillshotHandler)
+
+	update_player_ball_label()
 
 	if MPF.game.player:
 		var currentBall = MPF.game.player.ball
@@ -27,7 +54,95 @@ func _ready():
 	videoPlayer.finished.connect(_on_video_finished)
 	videoPlayer.play()
 
+
+func _exit_tree():
+	if clearSkillshotHandler.is_valid():
+		MPF.server.remove_event_handler("clear_skillshot_player_ball", clearSkillshotHandler)
+
+
+func update_player_ball_label():
+	if not playerBallLabel:
+		return
+
+	var player_num = 1
+	var ball_num = 1
+
+	if MPF.game.player:
+		ball_num = int(MPF.game.player.ball)
+
+		var possible_player_num = null
+
+		if "number" in MPF.game.player:
+			possible_player_num = MPF.game.player.number
+		elif "player_num" in MPF.game.player:
+			possible_player_num = MPF.game.player.player_num
+		elif "num" in MPF.game.player:
+			possible_player_num = MPF.game.player.num
+
+		if possible_player_num != null:
+			player_num = int(possible_player_num)
+
+	playerBallLabel.text = "PLAYER %d  BALL %d" % [player_num, ball_num]
+	playerBallLabel.visible = true
+
+	playerBallLabelActive = true
+	playerBallFlashTime = 0.0
+	playerBallFlashCount = 0
+	playerBallFlashFinished = false
+	playerBallHoldTime = 0.0
+	playerBallHolding = false
+
+
+func hide_player_ball_label():
+	playerBallLabelActive = false
+	playerBallFlashFinished = true
+	playerBallHolding = false
+	playerBallFlashTime = 0.0
+	playerBallFlashCount = 0
+	playerBallHoldTime = 0.0
+
+	if playerBallLabel:
+		playerBallLabel.visible = false
+		playerBallLabel.text = ""
+
+
+func flash_player_ball_label(delta):
+	if not playerBallLabel:
+		return
+
+	if not playerBallLabelActive:
+		playerBallLabel.visible = false
+		return
+
+	if not playerBallFlashFinished:
+		playerBallFlashTime += delta
+
+		if playerBallFlashTime >= playerBallFlashSpeed:
+			playerBallFlashTime = 0.0
+			playerBallLabel.visible = not playerBallLabel.visible
+			playerBallFlashCount += 1
+
+			if playerBallFlashCount >= playerBallMaxFlashes:
+				playerBallFlashFinished = true
+				playerBallHolding = true
+				playerBallHoldTime = 0.0
+				playerBallLabel.visible = true
+
+		return
+
+	if playerBallHolding:
+		playerBallLabel.visible = true
+		playerBallHoldTime += delta
+
+		if playerBallHoldTime >= playerBallHoldDuration:
+			hide_player_ball_label()
+
+		return
+
+
 func _process(delta):
+	flash_player_ball_label(delta)
+
 	if playingIntro:
 		return
 
@@ -37,6 +152,7 @@ func _process(delta):
 		position.x -= speed * delta
 
 	var screen_center = get_viewport_rect().size.x / 2
+
 	if abs(position.x - screen_center) < 150:
 		modulate = Color(1, 0, 0)
 		isInCenter = true
@@ -51,7 +167,10 @@ func _process(delta):
 		movingRight = true
 		flip_h = false
 
+
 func CheckHit(payload: Dictionary):
+	hide_player_ball_label()
+
 	if playingIntro:
 		MPF.server.send_event("skillshot_miss")
 		return
@@ -59,8 +178,6 @@ func CheckHit(payload: Dictionary):
 	if isInCenter:
 		hitSuccess = true
 
-		# Super Skill Shot starts instantly here.
-		# This is before the reward video finishes.
 		if not skillshotHitSent:
 			skillshotHitSent = true
 			MPF.server.send_event("skillshot_hit_fast")
@@ -79,13 +196,20 @@ func CheckHit(payload: Dictionary):
 	videoPlayer.visible = true
 	videoPlayer.play()
 
+
 func _on_video_finished():
 	if playingIntro:
 		playingIntro = false
 		videoPlayer.visible = false
 		return
 
+	hide_player_ball_label()
+
 	if hitSuccess:
 		MPF.server.send_event("skillshot_hit")
 	else:
 		MPF.server.send_event("skillshot_miss")
+
+
+func _on_clear_skillshot_player_ball(payload: Dictionary):
+	hide_player_ball_label()
